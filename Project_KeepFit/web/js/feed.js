7a1380a64c9b51cb5ae604e3a080d3af6fe44d0a
Eungu Han <ehan831@gmail.com>
2019-07-18 18:26:19 +0900

    no message

---
+++ Project_KeepFit/web/js/feed.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    // 좋아요 클릭 시, 하트 이미지 변경되는 메소드
+    let status = false;
+    $('img[name=likeIt]').click( function () {
+        if( status ) {
+            $(this).attr('src', 'img/heart.svg');
+        } else $(this).attr('src', 'img/heart_filled.svg');
+        status = !status;
+    });
+});
